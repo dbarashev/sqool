12e126f2649342e238220e3a17618075a863dee2
Dmitry Barashev <dbarashev@bardsoftware.com>
2019-03-21 22:25:43 +0300

    Show SQL style result spec in the table

--- src/main/ts/Task.ts
+++ src/main/ts/Task.ts
@@ -1,7 +1,12 @@
+export class ColumnSpec {
+    constructor(readonly name: string, readonly type: string) {}
+}
+
 export class TaskDto  {
     constructor(readonly id: number,
                 readonly name: string,
                 readonly description: string,
                 // tslint:disable-next-line
-                readonly result_json: string) {}
+                readonly result_json: string) {
+    }
 }
