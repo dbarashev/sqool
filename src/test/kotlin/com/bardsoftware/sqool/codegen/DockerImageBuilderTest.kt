eca489b1438b5ed828ada0268252ec29ff5350f5
Dmitry <qudeed@gmail.com>
2019-05-26 17:42:55 +0300

    Access postgres using localhost?

--- src/test/kotlin/com/bardsoftware/sqool/codegen/DockerImageBuilderTest.kt
+++ src/test/kotlin/com/bardsoftware/sqool/codegen/DockerImageBuilderTest.kt
@@ -13,7 +13,7 @@ import java.io.ByteArrayOutputStream
 class DockerImageBuilderTest {
     private val outputStream = ByteArrayOutputStream()
     private val flags = mock<Flags> {
-        on { postgresAddress } doReturn "postgres"
+        on { postgresAddress } doReturn "localhost"
         on { postgresPort } doReturn "5432"
         on { postgresUser } doReturn "postgres"
         on { postgresPassword } doReturn ""
